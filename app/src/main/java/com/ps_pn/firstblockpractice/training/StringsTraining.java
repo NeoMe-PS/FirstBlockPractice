a3dcbe49998ef007e18ec5c2f833f4e36a2d5616
Pavel Buyanov <neome.ps@gmail.com>
2024-02-10 21:55:38 +0400

    Закончил работу с :  - Работа со строками (StringsTraining.java в javacoreTraining-master.zip)

--- app/src/main/java/com/ps_pn/firstblockpractice/training/StringsTraining.java
+++ app/src/main/java/com/ps_pn/firstblockpractice/training/StringsTraining.java
@@ -1,5 +1,8 @@
 package com.ps_pn.firstblockpractice.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Набор тренингов по работе со строками в java.
  * <p>
@@ -11,70 +14,120 @@ package com.ps_pn.firstblockpractice.training;
  */
 public class StringsTraining {
 
-    /**
-     * Метод по созданию строки,
-     * состоящей из нечетных символов
-     * входной строки в том же порядке
-     * (нумерация символов идет с нуля)
-     *
-     * @param text строка для выборки
-     * @return новая строка из нечетных
-     * элементов строки text
-     */
-    public String getOddCharacterString(String text) {
-        //TODO: implement it
-        return "";
-    }
+	/**
+	 * Метод по созданию строки,
+	 * состоящей из нечетных символов
+	 * входной строки в том же порядке
+	 * (нумерация символов идет с нуля)
+	 *
+	 * @param text строка для выборки
+	 * @return новая строка из нечетных
+	 * элементов строки text
+	 */
+	public String getOddCharacterString(String text) {
+		if (text.isEmpty() || text.length() == 1) {
+			return "";
+		}
+		char[] charArray = text.toCharArray();
+		StringBuilder result = new StringBuilder();
+		for (int i = 1; i < charArray.length; i += 2) {
+			result.append(charArray[i]);
+		}
+		return result.toString();
+	}
 
-    /**
-     * Метод для определения количества
-     * символов, идентичных последнему
-     * в данной строке
-     *
-     * @param text строка для выборки
-     * @return массив с номерами символов,
-     * идентичных последнему. Если таких нет,
-     * вернуть пустой массив
-     */
-    public int[] getArrayLastSymbol(String text) {
-        //TODO: implement it
-        return new int[]{};
-    }
+	/**
+	 * Метод для определения количества
+	 * символов, идентичных последнему
+	 * в данной строке
+	 *
+	 * @param text строка для выборки
+	 * @return массив с номерами символов,
+	 * идентичных последнему. Если таких нет,
+	 * вернуть пустой массив
+	 */
+	public int[] getArrayLastSymbol(String text) {
 
-    /**
-     * Метод по получению количества
-     * цифр в строке
-     *
-     * @param text строка для выборки
-     * @return количество цифр в строке
-     */
-    public int getNumbersCount(String text) {
-        //TODO: implement it
-        return 0;
-    }
+		if (text.isEmpty() || text.length() == 1) {
+			return new int[]{};
+		}
 
-    /**
-     * Дан текст. Заменить все цифры
-     * соответствующими словами.
-     *
-     * @param text текст для поиска и замены
-     * @return текст, где цифры заменены словами
-     */
-    public String replaceAllNumbers(String text) {
-        //TODO: implement it
-        return text;
-    }
+		List<Integer> indexes = new ArrayList<>();
+		char lastSymbol = text.charAt(text.length() - 1);
+		for (int i = 0; i < text.length() - 1; i++) {
+			if (text.charAt(i) == lastSymbol) {
+				indexes.add(i);
+			}
+		}
 
-    /**
-     * Метод должен заменить заглавные буквы
-     * на прописные, а прописные на заглавные
-     *
-     * @param text строка для изменения
-     * @return измененная строка
-     */
-    public String capitalReverse(String text) {
-        //TODO: implement it
-        return text;
-    }
+		return indexes.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	/**
+	 * Метод по получению количества
+	 * цифр в строке
+	 *
+	 * @param text строка для выборки
+	 * @return количество цифр в строке
+	 */
+	public int getNumbersCount(String text) {
+
+		if (text != null) {
+			return text.replaceAll("\\D", "").length();
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Дан текст. Заменить все цифры
+	 * соответствующими словами.
+	 *
+	 * @param text текст для поиска и замены
+	 * @return текст, где цифры заменены словами
+	 */
+	public String replaceAllNumbers(String text) {
+
+		if (text != null) {
+			return text.replaceAll("1", "one")
+					.replaceAll("2", "two")
+					.replaceAll("3", "three")
+					.replaceAll("4", "four")
+					.replaceAll("5", "five")
+					.replaceAll("6", "six")
+					.replaceAll("7", "seven")
+					.replaceAll("8", "eight")
+					.replaceAll("9", "nine")
+					.replaceAll("0", "zero");
+		} else {
+			return null;
+		}
+
+	}
+
+	/**
+	 * Метод должен заменить заглавные буквы
+	 * на прописные, а прописные на заглавные
+	 *
+	 * @param text строка для изменения
+	 * @return измененная строка
+	 */
+	public String capitalReverse(String text) {
+
+		if (text == null) {
+			return null;
+		}
+
+		char[] chars = text.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			if (Character.isUpperCase(c)) {
+				chars[i] = Character.toLowerCase(c);
+			} else if (Character.isLowerCase(c)) {
+				chars[i] = Character.toUpperCase(c);
+			}
+		}
+		return String.valueOf(chars);
+	}
 
 }
