a717ca4935b09bbac9b2de487404edac2fe6658a
Pavel Buyanov <neome.ps@gmail.com>
2024-02-22 19:21:09 +0400

    - cherry-pick из layouts

---
+++ app/src/main/java/com/ps_pn/firstblockpractice/presentation/fragments/news/NewsFragment.kt
@@ -0,0 +1,27 @@
+package com.ps_pn.firstblockpractice.presentation.fragments.news
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ps_pn.firstblockpractice.databinding.FragmentNewsBinding
+
+class NewsFragment : Fragment() {
+    private var _binding: FragmentNewsBinding? = null
+    private val binding
+        get() = _binding ?: throw RuntimeException("FragmentNewsBinding is null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/ps_pn/firstblockpractice/presentation/fragments/search/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.ps_pn.firstblockpractice.presentation.fragments.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ps_pn.firstblockpractice.R
+
+class SearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+}
